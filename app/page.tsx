ed16cbcae4bbf7301a5385da4ab561b2538de7c7
Grenzlinie <calvin_lsy@163.com>
2025-03-24 11:32:47 +0800

    update: add google analytics for tracking daily users

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+'use client';
+import ReactGA from 'react-ga4';
+import { useEffect } from 'react';
 import Introduction from '@/components/home/Introduction';
 import Profile from '@/components/home/Profile'; // Personal profile component
 import SelectedPapers from '@/components/home/Selected'; // Selected papers component
 import JoinUs from '@/components/home/JoinUs'; // Join Us component
 
+const TRACKING_ID = 'G-2CZYC6X9SY';
+
 export default function Home() {
+  useEffect(() => {
+    if (TRACKING_ID) {
+      ReactGA.initialize(TRACKING_ID);
+      ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
+    }
+  }, []);
+  
   return (
     <div className="space-y-8">
       <Profile />
